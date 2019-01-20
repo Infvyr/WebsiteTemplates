fad4e79f1ee0549595826e915fc42647f0c803d0
Infvyr <allfortrashmail@gmail.com>
2019-01-20 21:45:13 +0200

    Updated custom.js

--- Hekwerk/assets/js/custom.js
+++ Hekwerk/assets/js/custom.js
@@ -1,3 +1,5 @@
+// sticky menu
+
 window.onscroll = function() { makeMenuSticky() };
 
 var querySticky = document.querySelector("#sticky-container");
@@ -12,7 +14,19 @@ function makeMenuSticky() {
 
   if( window.scrollY >= sticky ) {
   	querySticky.classList.add("sticky");
+    document.querySelector('.text-block').style.marginTop = 120 + 'px';
   } else {
     querySticky.classList.remove("sticky");
+    document.querySelector('.text-block').style.marginTop = 0 + 'px';
   }
 }
+
+
+// counter js
+(function($){
+  $('.counter-number').counterUp({
+    time: 800
+});
+
+
+})(jQuery);
