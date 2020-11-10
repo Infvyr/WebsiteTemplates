504e07e86cf5dde0d528c151696bea6828da9516
Infvyr <allfortrashmail@gmail.com>
2020-11-10 23:13:59 +0200

    Fix mobile toggle menu and filter widgets

--- Product Filter/js/sidebar.js
+++ Product Filter/js/sidebar.js
@@ -1,21 +1,85 @@
 document.addEventListener("DOMContentLoaded", initSidebar, false);
 
 function initSidebar(){
-    const seeMoreButton = document.getElementById('see-more');
+    
+    // see more button
+    const seeMoreBtn = document.getElementById('see-more');
+    const widgetElements = document.querySelectorAll('.widget-options .widget__element--hidden');
 
-    seeMoreButton.addEventListener('click', () => {
-       const widgetElement = Array.from(document.querySelectorAll('.widget-options .hidden'));
+    if (!seeMoreBtn || !widgetElements) return;
 
-       if (!widgetElement) return;
+    seeMoreBtn.addEventListener('click', (e) => {
+        e.preventDefault();
 
-       for (const elem of widgetElement) {
-           if (elem.classList.contains('hidden')) {
-               elem.classList.remove('hidden');
+        if(seeMoreBtn.dataset.option === 'hidden'){
+            widgetElements.forEach(widgetElem => {
+                widgetElem.style.display = 'block';
+            });
+
+            seeMoreBtn.textContent = 'Close options';
+            seeMoreBtn.dataset.option = 'visible';
+
+        } else {
+            widgetElements.forEach(widgetElem => {
+                widgetElem.style.display = 'none';
+            });
+
+            seeMoreBtn.textContent = 'See more';
+            seeMoreBtn.dataset.option = 'hidden';
+        }
+        
+    });
+
+
+    // toggle sidebar filter on mobile
+    document.addEventListener('click', function (event) {
+        const toggleFilterBtn = document.getElementById('toggle-filter');
+        const sidebar = document.querySelector('.sidebar');
+
+        // If the click happened inside the the container, bail
+        if (event.target.closest('.sidebar')) return;
+    
+        toggleFilterBtn.classList.toggle('is-active');
+                sidebar.classList.toggle('active');
+                document.querySelector('section.container').classList.toggle('active');
+    
+    }, false);
+
+
+    // sidebar widget toggle
+    const widgets = document.querySelectorAll('.form-filter .widget');
+
+    if (!widgets) return;
+
+    widgets.forEach(widget => {
+        widget.addEventListener('click', e => {
+            if(e.target.classList.contains('widget__title')){
+                e.target.classList.toggle('widget__title--active');
+                e.target.nextElementSibling.classList.toggle('widget__body--closed');
             }
+        });
+    });
 
-            if(!elem.classList.contains('hidden')){
-                seeMoreButton.remove();
+
+    // sidebar widget location
+    const locationCheckboxAny = document.getElementById('location-any');
+    const locationTopCheckboxes = document.querySelectorAll('.location__checkbox:not(#location-any)');
+
+    if (!locationCheckboxAny || !locationTopCheckboxes) return;
+
+    locationCheckboxAny.addEventListener('change', () => {
+        if(locationCheckboxAny.checked){
+            locationTopCheckboxes.forEach(checkboxItem => {
+                checkboxItem.checked = false;
+            });
+        }
+    });
+
+    locationTopCheckboxes.forEach(checkboxItem => {
+        checkboxItem.addEventListener('change', () => {
+            if(locationCheckboxAny.checked){
+                locationCheckboxAny.checked = false;
             }
-       }
+        });
     });
 }
